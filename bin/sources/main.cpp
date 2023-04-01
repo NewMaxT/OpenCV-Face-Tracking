204fec0dbf36ad3189590e561ddc036777010d65
Max <maxencegoutteratelspotify@gmail.com>
2023-04-01 19:44:40 -0400

    Adding base files

---
+++ bin/sources/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Made by Maxence Goutteratel
+ * https://github.com/NewMaxT/OpenCV-Face-Tracking
+ * LICENCE TBD
+ * */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf ("%s\n", "Hello world");
+}
+
+
+
+
